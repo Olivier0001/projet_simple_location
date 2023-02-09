e0a3d91a7b44b1c0b3fa148ff0c4b39d4cc564f1
Olivier MALAMOU <olivier.malamou@syd.fr>
2023-02-09 13:15:04 +0100

    CRUD Clients OK

---
+++ SLApps/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SLApps.Model;
+using System.Collections.Generic;
+
+namespace SLApps.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ SLApps/Model/Client.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SLApps.Model
+{
+    public class Client
+    {
+        [Key]
+        public int id_client { get; set; }
+        public int id_role { get; set; } = 1;
+
+
+        [Required(ErrorMessage = "Le champ Nom Client est obligatoire")]
+        public string nom_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Prenom Client est obligatoire")]
+        public string prenom_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Age Client est obligatoire")]
+        public string age_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Numero Telephone Client est obligatoire")]
+        public string numero_telephone_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Email Client est obligatoire")]
+        public string email_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Adresse Client est obligatoire")]
+        public string adresse_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Code Postal Client est obligatoire")]
+        public string code_postal_client { get; set; }
+
+        [Required(ErrorMessage = "Le champ Ville Client est obligatoire")]
+        public string ville_client { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime date_creation_client { get; set; } = DateTime.Now;
+    }
+}
