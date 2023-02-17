3708e30c9dafdeb096fd49f95fdec3f4ed016701
Olivier MALAMOU <olivier.malamou@syd.fr>
2023-02-17 17:56:28 +0100

    Ajout du lien de l'image de la voiture dans le formulaire edit de la voiture

---
+++ SLAppsModels/Images_Voiture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAppsModels
+{
+    public class Images_Voiture
+    {
+        public int id_image_voiture { get; set; }
+        public string nom_image_voiture { get; set; }
+        public DateTime date_creation_image_voiture { get; set; } = DateTime.Now;
+    }
+}
